c5ea7c210744d55180fbd23c3af614a79eb65dae
docube <segunolawepo@rocketmail.com>
2025-05-15 13:46:23 +0100

    Yes

--- frontend/documentor-frontend/vite.config.ts
+++ frontend/documentor-frontend/vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Allows access from external devices
+    port: 5173,      // You can change this if needed
+  },
 })
